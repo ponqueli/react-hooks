463aee9ae4648c3270fa1f10df2aa7b8c2b7cb22
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-03-05 23:15:24 -0300

    hook useState com inteiro e string. Inclusão de animacoes e hover

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -4,9 +4,13 @@ import SectionTitle from "../../components/layout/SectionTitle";
 import PageTitle from "../../components/layout/PageTitle";
 
 const UseState = (props) => {
-  const [countCode, setCountCode] = useState(`const [count, setCount] = useState(0);`)
-  const [nameCode, setNameCode] = useState(`const [name, setName] = useState("");`)
-  
+  const [countCode, setCountCode] = useState(
+    `const [count, setCount] = useState(0);`
+  );
+  const [nameCode, setNameCode] = useState(
+    `const [name, setName] = useState("");`
+  );
+
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
 
@@ -24,30 +28,63 @@ const UseState = (props) => {
         <div>
           <button
             className="btn hvr-wobble-vertical"
+            onClick={() => setCount((current) => current + 1000)}
+          >
+            +1000
+          </button>
+          <button
+            className="btn hvr-wobble-vertical"
+            onClick={() => setCount(count + 100)}
+          >
+            +100
+          </button>
+          <button
+            className="btn hvr-wobble-vertical"
             onClick={() => setCount(count + 1)}
           >
             +1
           </button>
           <button
-            className="btn hvr-wobble-vertical"
+            className="btn hvr-wobble-vertical bc-gray"
+            onClick={() => setCount(0)}
+          >
+            0
+          </button>
+          <button
+            className="btn hvr-wobble-vertical bc-red"
             onClick={() => setCount(count - 1)}
           >
             -1
           </button>
+          <button
+            className="btn hvr-wobble-vertical bc-red"
+            onClick={() => setCount(count - 100)}
+          >
+            -100
+          </button>
+          <button
+            className="btn hvr-wobble-vertical bc-red"
+            onClick={() => setCount((current) => current - 1000)}
+          >
+            -1000
+          </button>
         </div>
       </div>
-      <pre style={{display:'flex', flexDirection: 'column', fontSize: '3rem'}}>
+      <pre
+        style={{ display: "flex", flexDirection: "column", fontSize: "3rem" }}
+      >
         <code className="hvr-skew-forward">{countCode}</code>
       </pre>
 
       <SectionTitle title="Exercício #2" />
-      
+
       <input
         type="text"
         className="input"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <span className="text hvr-skew-forward">{name}</span>
     </div>
   );
 };
