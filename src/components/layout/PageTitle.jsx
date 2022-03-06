a8edb76725b4f376dd603190ff1d392a185d3280
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-03-05 21:29:48 -0300

    add hover no titulo

--- src/components/layout/PageTitle.jsx
+++ src/components/layout/PageTitle.jsx
@@ -2,7 +2,7 @@ import './PageTitle.css'
 import React from 'react'
 
 const PageTitle = props => (
-    <div className={`PageTitle ${props.error ? "error" : ""}`}>
+    <div className={`hvr-grow PageTitle ${props.error ? "error" : ""}`}>
         <h1>{props.title}</h1>
         <h2>{props.subtitle}</h2>
     </div>
