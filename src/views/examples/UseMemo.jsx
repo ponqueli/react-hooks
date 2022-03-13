544265ec7629de62725b3610fa98347ae83d2b83
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-03-13 19:45:17 -0300

    hook useContext melhor opcao de uso + segura

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import changeTitle from "../../functions/useDocumentTitle";
 import PageTitle from "../../components/layout/PageTitle";
 
